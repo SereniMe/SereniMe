91c8a52197431d4c9ce70f3fa9cb61af70c493a9
alderrr <alifdermayudha@gmail.com>
2023-12-11 18:18:25 +0700

    feat: added-model-user-register

---
+++ sereni-me/src/db/models/userRegister.ts
@@ -0,0 +1,31 @@
+import { ObjectId } from "mongodb";
+import { getMongoClientInstance } from "../config";
+import { hashPassword } from "@/utils/bcryptjs";
+
+export type UserModel = {
+  _id: ObjectId;
+  name: string;
+  username: string;
+  email: string;
+  password: string;
+};
+export type UserModelCreateInput = Omit<UserModel, "_id">;
+
+const DATABASE_NAME = process.env.MONGO_DB_NAME;
+const COLLECTION_USER = "users";
+
+export const getDB = async () => {
+  const client = await getMongoClientInstance();
+  const db = client.db(DATABASE_NAME);
+  return db;
+};
+
+export const createUser = async (user: UserModelCreateInput) => {
+  const newUser: UserModelCreateInput = {
+    ...user,
+    password: hashPassword(user.password),
+  };
+  const db = await getDB();
+  const result = await db.collection(COLLECTION_USER).insertOne(newUser);
+  return result;
+};
