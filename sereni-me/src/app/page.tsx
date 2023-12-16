20c16ad79a0dadfe60f5d02bcfd7ffda54e1d3a1
“RDHSAHS” <rdwikaharris@gmail.com>
2023-12-16 21:10:10 +0700

    testing-landing-page

--- sereni-me/src/app/page.tsx
+++ sereni-me/src/app/page.tsx
@@ -1,18 +1,18 @@
 import Image from "next/image";
-import {CafeScene} from "./(3dscenes)/CafeScene";
-import {WinterScene} from "./(3dscenes)/WinterScene";
-import {LighthouseScene} from "./(3dscenes)/LighthouseScene";
-import {ForestCampScene} from "./(3dscenes)/ForestCamp";
-import {StreetScene} from "./(3dscenes)/StreetScene";
+import { CafeScene } from "./(3dscenes)/CafeScene";
+import { WinterScene } from "./(3dscenes)/WinterScene";
+import { LighthouseScene } from "./(3dscenes)/LighthouseScene";
+import { ForestCampScene } from "./(3dscenes)/ForestCamp";
+import { StreetScene } from "./(3dscenes)/StreetScene";
 
 export default function Home() {
-	return (
-		<main>
-			{/* <CafeScene /> */}
-			{/* <WinterScene /> */}
-			{/* <LighthouseScene /> */}
-			{/* <ForestCampScene /> */}
-			<StreetScene />
-		</main>
-	);
+  return (
+    <main>
+      {/* <CafeScene /> */}
+      {/* <WinterScene /> */}
+      {/* <LighthouseScene /> */}
+      {/* <ForestCampScene /> */}
+      <StreetScene />
+    </main>
+  );
 }
