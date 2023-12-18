a291ba5992e8d1390b2191ff444d5e558102873c
fionadrsnt <project.fionadarasanti@gmail.com>
2023-12-19 04:05:55 +0700

    profile on progress

--- sereni-me/src/app/cms/users/page.tsx
+++ sereni-me/src/app/cms/users/page.tsx
@@ -18,7 +18,6 @@ const UserPage: React.FC = () => {
                   <col />
                   <col />
                   <col />
-                  <col />
                   <col className="w-24" />
                 </colgroup>
                 <thead className="dark:bg-gray-700">
@@ -27,7 +26,6 @@ const UserPage: React.FC = () => {
                     <th className="p-3 border border-gray-300">Name</th>
                     <th className="p-3 border border-gray-300">Username</th>
                     <th className="p-3 border border-gray-300">Email</th>
-                    <th className="p-3 border border-gray-300">Action</th>
                   </tr>
                 </thead>
                 <tbody>
@@ -43,20 +41,6 @@ const UserPage: React.FC = () => {
                     </td>
                     <td className="p-3 text-center">
                       <p>user1@mail.com</p>
-                    </td>
-                    <td className="py-2 px-4 text-right">
-                      <div className="font-semibold rounded-md dark:bg-violet-400 dark:text-gray-900 flex flex-row justify-center items-center">
-                        <div className="mr-5">
-                          <button className="bg-blue-700 hover:bg-gray-500 rounded-md text-white font-bold py-2 px-4 border-b-4 border-gray-700 hover:text-black hover:scale-110 transition-all active:scale-90 ">
-                            Edit
-                          </button>
-                        </div>
-                        <div className="">
-                          <button className="bg-red-700 hover:bg-gray-400 rounded-md text-white font-bold py-2 px-4 border-b-4 border-gray-700 hover:text-black hover:scale-110 transition-all active:scale-90 ">
-                            Delete
-                          </button>
-                        </div>
-                      </div>
                     </td>
                   </tr>
                 </tbody>
