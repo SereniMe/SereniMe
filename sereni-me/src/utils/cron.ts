d67bb5239f7969b1c8d0f8eef1347d644f85f552
alderrr <alifdermayudha@gmail.com>
2023-12-20 18:29:04 +0700

    finish layouting email and cron timing at 7PM

--- sereni-me/src/utils/cron.ts
+++ sereni-me/src/utils/cron.ts
@@ -23,7 +23,7 @@ const { CronJob } = require("cron");
 // };
 
 const job = new CronJob(
-  "* * * * * *", // cronTime (07:00) everyday
+  "0 19 * * *", // cronTime (07:00) everyday
   async () => {
     const profiles = await fetchProfiles();
     const activities = await fetchActivities();
@@ -32,8 +32,13 @@ const job = new CronJob(
     const foundactivity = profiles.map((profile) => {
       const email = profile.email;
       const activity = activities.find((activity) =>
-        profile.interests.split(",").includes(activity.tags[0])
+        profile.interests
+          .split(",")
+          .includes(
+            activity.tags[Math.floor(Math.random() * activity.tags.length)]
+          )
       );
+
       return { email, activity };
     });
     console.log(foundactivity, "<<<<<<<<<< ");
@@ -85,77 +90,82 @@ const sendEmail = async (recipients) => {
       html: "",
     };
     recipients.forEach((userContent) => {
+      const id = userContent?.activity?.thumbnail?.split("/");
       options.to = userContent.email;
-      options.html = `
-      <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Email Notification</title>
-  </head>
-  <body
-    style="
-      font-family: 'Arial', sans-serif;
-      background-color: #f0f0f0;
-      text-align: center;
-      padding: 20px;
-    "
-  >
-    <div
-      style="
-        max-width: 600px;
-        margin: 0 auto;
-        background-color: #ffffff;
-        border-radius: 8px;
-        overflow: hidden;
-        box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-        display: flex;
-        justify-content: center;
-      "
-    >
-      <h1
+      if (id) {
+        options.html = `
+        <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Email Notification</title>
+      </head>
+      <body
         style="
-          /* background-color: #7b746b; */
-          /* width: 85%; */
-          text-shadow: 2px 2px 4px #000000;
-          color: #f4b81f;
-          padding: 30px;
-          margin: 0;
-          position: absolute;
-          /* box-shadow: #f0f0f0 10px; */
+          font-family: 'Arial', sans-serif;
+          background-color: #f0f0f0;
+          text-align: center;
+          padding: 20px;
         "
       >
-        SereniMe's <br />Activity Recommendation
-      </h1>
-      <a
-        href="http://localhost:3000"
-        style="display: block; text-decoration: none"
-      >
-        <img
-          src="https://www.calm.com/_next/image?url=%2F_n%2Fimages%2Fhomepage%2Fjasper-lake-hero-banner.webp&w=1920&q=75"
-          alt="Mountain Lake View"
-          style="max-width: 100%; height: auto"
-        />
-      </a>
-    </div>
-    <div style="display: flex; flex-direction: column; align-items: center">
-      <h1>Hey ${userContent.email}!</h1>
-      <p>Try "Activity Name" today and tell us how you feel (•ᴗ•)</p>
-      <img
-        src="https://d1vbn70lmn1nqe.cloudfront.net/prod/wp-content/uploads/2021/06/29044522/Yoga.jpg.webp"
-        alt=""
-        style="max-width: 50vw; height: auto"
-      />
-      <br /><br />
-      <h3>"Activity Name"</h3>
-      <p style="padding-left: 20px; max-width: 50vw">
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque
-        eaque porro aut ex quasi consectetur laudantium error laboriosam
-        similique, facilis ipsam non accusamus dolor dolorum, nostrum distinctio
-        vel, dignissimos eum?
-      </p>
-    </div>
-  </body>
+        <div
+          style="
+            max-width: 600px;
+            margin: 0 auto;
+            background-color: #ffffff;
+            border-radius: 8px;
+            overflow: hidden;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            position: relative;
+          "
+        >
+          <h1
+            style="
+              text-shadow: 2px 2px 4px #000000;
+              color: #70605b;
+              padding: 30px;
+              margin: 0;
+              position: absolute;
+            "
+          >
+            SereniMe's <br />Activity Recommendation
+          </h1>
+    
+          <a
+            href="http://localhost:3000"
+            style="text-decoration: none; display: block"
+          >
+            <img
+              src="https://www.calm.com/_next/image?url=%2F_n%2Fimages%2Fhomepage%2Fjasper-lake-hero-banner.webp&w=1920&q=75"
+              alt="Mountain Lake View"
+              style="max-width: 100%; height: auto"
+            />
+          </a>
+        </div>
+    
+        <div style="display: block; text-align: center">
+          <h1>Hey ${userContent.email}!</h1>
+          <p>
+            Try "${userContent.activity?.name}" today and tell us how you feel (•ᴗ•)
+          </p>
+    
+          <img
+            src="https://drive.google.com/uc?export=view&id=${id[5]}"
+            alt=""
+            style="max-width: 50vw; height: auto"
+          />
+    
+          <br /><br />
+    
+          <h3>"${userContent.activity?.name}"</h3>
+    
+          <h3>${userContent.activity?.content}</h3>
+            
+          </p>
+        </div>
+      </body>
         `;
+      }
+
       transporter.sendMail(options, (error, info) => {
         if (error) {
           console.log(error);
